7bd9bdce52174428a4026049e539919195da0df1
Brendan Lestander <blestander@pitt.edu>
2018-12-03 12:02:12 -0500

    Implemented Change Food page visuals.

--- nutrition_tracker/lib/pages/change_food.dart
+++ nutrition_tracker/lib/pages/change_food.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:nutrition_tracker/user.dart';
+import 'package:intl/intl.dart';
 import 'package:firebase_database/firebase_database.dart';
+
+import 'package:nutrition_tracker/user.dart';
+import 'package:nutrition_tracker/fooditem.dart';
+import 'package:nutrition_tracker/dailycal.dart';
 
 class ChangeFoodPage extends StatelessWidget {
   User _user;
   final reference = FirebaseDatabase.instance.reference();
+  final DateTime today;
 
-  ChangeFoodPage(this._user);
+  ChangeFoodPage(this._user) :
+        today = _stripTime(DateTime.now());
+
+  static DateTime _stripTime(DateTime dt) {
+    return new DateTime(dt.year, dt.month, dt.day);
+  }
 
   Future<bool> _back(BuildContext context) async {
     await reference.child(_user.displayName).set(_user.toJson());
@@ -20,8 +30,41 @@ class ChangeFoodPage extends StatelessWidget {
             appBar: new AppBar(
                 title: Text("Change Foods")
             ),
-            body: new Text("Test")
+            body: new ListView(
+              children: <Widget>[
+                buildDayList(context, today),
+                buildDayList(context, today.subtract(Duration(days: 1))),
+                buildDayList(context, today.subtract(Duration(days: 2))),
+                buildDayList(context, today.subtract(Duration(days: 3))),
+                buildDayList(context, today.subtract(Duration(days: 4))),
+                buildDayList(context, today.subtract(Duration(days: 5))),
+                buildDayList(context, today.subtract(Duration(days: 6))),
+              ],
+              shrinkWrap: true,
+            )
         )
+    );
+  }
+
+  buildDayList(BuildContext context, DateTime day) {
+    List<FoodItem> foodItems = _user.dailyCal.items[day];
+    List<ListTile> foodRows = foodItems != null ? foodItems.map((item) {
+      return new ListTile(
+          title: new Text(item.name),
+          trailing: new Icon(Icons.clear)
+      );
+    }).toList() : [];
+    List<Widget> listChildren = <Widget>[
+      new Text(
+          new DateFormat.MMMMd().format(day),
+          textAlign: TextAlign.center,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)
+      ),
+    ];
+    listChildren.addAll(foodRows);
+    return new ListView(
+      children: listChildren,
+      shrinkWrap: true,
     );
   }
 }
